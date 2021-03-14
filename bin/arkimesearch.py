5dfd73c3885f5686f662312d114089aafa1fdbd2
Your Name <christopher.costa@gmail.com>
2021-03-13 19:15:10 -0500

    Support multiple Elastic clusters

--- bin/arkimesearch.py
+++ bin/arkimesearch.py
@@ -8,7 +8,11 @@ from splunklib.six.moves import range
 @Configuration()
 class ArkimeSearchCommand(GeneratingCommand):
 
+    # TODO: Proper configuration settings
     prefix = 'arkime'
+    limit = 1000
+    elastic_nodes = [['http://elastic1-1:9200', 'http://elastic1-2:9200'],
+                     ['http://elastic2-1:9200', 'http://elastic2-2:9200']]
 
     def handle_results(self, result, prefix, record):
         if isinstance(result, dict):
@@ -29,17 +33,19 @@ class ArkimeSearchCommand(GeneratingCommand):
         return seconds + milliseconds / 1000
 
     def generate(self):
-        es = Elasticsearch(['http://elastic1-1:9200'])
+        # TODO: multithreading?
+        for nodelist in self.elastic_nodes:
+          es = Elasticsearch(nodelist)
 
-        results = es.search(index="sessions*")
+          results = es.search(index="sessions*", body={"from": 0, "size": self.limit})
 
-        for result in results['hits']['hits']:
-            record = {}
-            record = self.handle_results(result, self.prefix, record)
+          for result in results['hits']['hits']:
+              record = {}
+              record = self.handle_results(result, self.prefix, record)
 
-            record['_time'] = self.timestamp(record['arkime._source.firstPacket'])
-            record['_raw'] = str(record)
+              record['_time'] = self.timestamp(record['arkime._source.firstPacket'])
+              record['_raw'] = str(record)
 
-            yield record
+              yield record
  
 dispatch(ArkimeSearchCommand, sys.argv, sys.stdin, sys.stdout, __name__)
