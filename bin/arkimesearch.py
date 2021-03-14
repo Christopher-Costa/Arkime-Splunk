db4a76f4ae4e4293b3c0710320207fd559a88ff4
Your Name <christopher.costa@gmail.com>
2021-03-13 19:33:38 -0500

    Simplify timestamp computation; Add host

--- bin/arkimesearch.py
+++ bin/arkimesearch.py
@@ -11,8 +11,8 @@ class ArkimeSearchCommand(GeneratingCommand):
     # TODO: Proper configuration settings
     prefix = 'arkime'
     limit = 1000
-    elastic_nodes = [['http://elastic1-1:9200', 'http://elastic1-2:9200'],
-                     ['http://elastic2-1:9200', 'http://elastic2-2:9200']]
+    elastic_nodes = [['elastic1-1', 'elastic1-2'],
+                     ['elastic2-1', 'elastic2-2']]
 
     def handle_results(self, result, prefix, record):
         if isinstance(result, dict):
@@ -28,14 +28,12 @@ class ArkimeSearchCommand(GeneratingCommand):
         return record
      
     def timestamp(self, time):
-        seconds = int(time) / 1000
-        milliseconds = int(time) % 1000
-        return seconds + milliseconds / 1000
+        return time / 1000
 
     def generate(self):
         # TODO: multithreading?
         for nodelist in self.elastic_nodes:
-          es = Elasticsearch(nodelist)
+          es = Elasticsearch(nodelist, scheme="http", port="9200")
 
           results = es.search(index="sessions*", body={"from": 0, "size": self.limit})
 
@@ -43,6 +41,7 @@ class ArkimeSearchCommand(GeneratingCommand):
               record = {}
               record = self.handle_results(result, self.prefix, record)
 
+              record['host'] = nodelist[0]
               record['_time'] = self.timestamp(record['arkime._source.firstPacket'])
               record['_raw'] = str(record)
 
